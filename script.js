a05cde0d62ae5aa228a24c2a1c78be3cd9017f61
Nario <mario.forgac@educanet.cz>
2023-12-10 16:56:21 +0100

    Hover and random names done

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+function randomNum(max){
+    return Math.floor(Math.random() * max);
+}
+//listofNames[randomNum(9)]
+let listofNames = ["okeey", "Some name", "Name", "no idea", "JavaScript", "Html", "CSS", "OK", "K"]
+
+console.log(listofNames[randomNum(9)])
 let table = document.createElement("table");
 for (let i = 0; i < 10; i++) {
     let row = document.createElement("tr");
-
+        row.classList.add("hover")
     for (let j = 0; j < 3; j++) {
         let cell = document.createElement("td");
-        cell.textContent = "Řádek " + (i + 1);
+        cell.textContent = listofNames[randomNum(9)];
         row.appendChild(cell);
     }
 
